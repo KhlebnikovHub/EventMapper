4a6fd10637aca0e08aedaf1d00e40240a3014564
Vladimir <khlebnikovvladimir19@gmail.com>
2021-09-05 02:58:50 +0300

    new

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,21 @@ const { initUser } = require('../middlewares/initUser');
 router.route('/')
   .get(initUser, async (req, res) => {
     try {
+      let user_id;
       if (req?.session?.passport) {
         res.locals.name = req.session.passport.user.displayName;
         res.locals.img = req.session.passport.user.photos[0].value;
-      }
-        const user_id = req.session.passport.user.local_id;
+         user_id = req.session.passport.user.local_id;
+     
+        
         var myDate = new Date('2016-06-18 05:18:27')
         console.log('THIS DATE>>>>>>>>>', new Date())
         const futureEvents = await Event.findAll({ order: [['date', 'ASC']], where: { user_id, date: { [Op.gt]: new Date(), } }, limit: 6 })
         const lastEvents = await Event.findAll({ order: [['date', 'DESC']], where: { user_id, date: { [Op.lt]: new Date(), } }, limit: 6 })
         console.log('future EVENTS >>>>>>>>>>>>>>>>>', futureEvents);
-
-      res.render('index', { futureEvents, lastEvents });
+        res.render('index', { futureEvents, lastEvents });
+ } else res.render('index');  
+      
     } catch (error) {
       console.log(error);
       return res.sendStatus(500).end();
